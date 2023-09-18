c33ff318035a7641a2f26e3c442f652417eaa96b
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-09-19 05:00:53 +0900

    fix:

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const owner = require('./routes/owner');
 const carInfo = require('./routes/carInfo');
-
+const ejsLayouts = require('express-ejs-layouts');
+const home = require('./routes/home')
 
 const app = express();
+
 app.use(morgan('tiny'));
+app.use(ejsLayouts);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.set('layout', "layouts/layout");
 app.use(express.json());
 app.use('/api/owner', owner);
-app.use('/api/carInfo', carInfo)
+app.use('/api/carInfo', carInfo);
+
+app.use('/', home);
 
 mongoose.connect('mongodb://127.0.0.1/Car')
   .then(() => {
